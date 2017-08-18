7c228f8eba6a6b4146b79d5f4505349cbb915a59
nandananr <nandana.nalapareddy@gmail.com>
2017-08-18 11:41:43 -0500

    change 3.1

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace SampleHelloWorldWeb
+namespace HelloWorld
 {
     public class Program
     {
--- src/HelloWorld/Startup.cs
+++ src/HelloWorld/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace SampleHelloWorldWeb
+namespace HelloWorld
 {
     public class Startup
     {
