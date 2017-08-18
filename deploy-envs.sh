35305826f8af384a11032ec900359ba7d4783d5e
nandananr <nandana.nalapareddy@gmail.com>
2017-08-18 12:17:28 -0500

    change 4

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,9 +4,10 @@
 export IMAGE_NAME=ecssample-helloworld
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=18.220.138.117
+export AWS_VIRTUAL_HOST=34.231.5.225
+#18.220.138.117
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
